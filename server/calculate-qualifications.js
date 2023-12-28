45dca6fd48f172347d288635ccf1fc9bac1e36ac
William Boomer Baker <boomer@boomerbaker.com>
2023-12-28 02:00:34 -0800

    Switching to node fetch

--- server/calculate-qualifications.js
+++ server/calculate-qualifications.js
@@ -1,14 +1,17 @@
-const https = require('https');
+const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
   const API_KEY = '0db948a6-50f1-d9f3-4579-4f8036dc3830';
-  const BASE_URL = 'api.forthcrm.com';
-  const contactId = JSON.parse(event.body).contact_id;
+  const BASE_URL = 'https://api.forthcrm.com/v1/contacts';
 
-  console.log('Received request with contact_id:', contactId);
+  // Extract the contact ID from the incoming request body
+  const body = JSON.parse(event.body);
+  const contactId = body.contact_id; // Ensure 'contact_id' matches the key in the incoming request
+
+  console.log('Received request with contact_id:', contactId); // Added console log
 
   if (!contactId) {
-    console.error('Contact ID is required');
+    console.error('Contact ID is required'); // Added console error
     return {
       statusCode: 400,
       body: JSON.stringify({ error: 'Contact ID is required' }),
@@ -16,25 +19,25 @@ exports.handler = async (event, context) => {
     };
   }
 
-  const API_PATH = `/v1/contacts/${contactId}/debts/enrolled`;
-  const options = {
-    hostname: BASE_URL,
-    path: API_PATH,
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'API-Key': API_KEY,
-    },
-  };
-
-  console.log('Making GET request to:', API_PATH);
+  const API_URL = `${BASE_URL}/${contactId}/debts/enrolled`;
+  console.log('API URL:', API_URL); // Added console log
 
   try {
-    const response = await performHttpRequest(options);
-    console.log('Response:', response);
+    console.log('Making GET request to:', API_URL); // Added console log
+    const response = await fetch(API_URL, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'API-Key': API_KEY,
+      },
+    });
+
+    const data = await response.json();
+
+    console.log('Response:', data); // Added console log
 
     // Process the response to calculate the total debt amount for credit card or unsecured debts
-    const debts = JSON.parse(response).response;
+    const debts = data.response;
     const totalDebt = debts.reduce((acc, debt) => {
       if (debt.notes.includes('CreditCard') || debt.notes.includes('Unsecured')) {
         return acc + parseFloat(debt.current_debt_amount);
@@ -42,45 +45,21 @@ exports.handler = async (event, context) => {
       return acc;
     }, 0);
 
-    console.log('Calculated total debt:', totalDebt);
+    console.log('Calculated total debt:', totalDebt); // Added console log
 
+    // Return the calculated total debt
     return {
       statusCode: 200,
       body: JSON.stringify({ totalDebt: totalDebt.toFixed(2) }),
       headers: { 'Access-Control-Allow-Origin': '*' },
     };
   } catch (error) {
-    console.error('Error:', error.message);
+    // Handle fetch errors
+    console.error('Error:', error.message); // Added console error
     return {
-      statusCode: error.statusCode || 500,
+      statusCode: 500,
       body: JSON.stringify({ error: error.message }),
       headers: { 'Access-Control-Allow-Origin': '*' },
     };
   }
 };
-
-function performHttpRequest(options) {
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, (res) => {
-      let data = '';
-
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      res.on('end', () => {
-        if (res.statusCode === 200) {
-          resolve(data);
-        } else {
-          reject({ statusCode: res.statusCode, message: data });
-        }
-      });
-    });
-
-    req.on('error', (error) => {
-      reject({ statusCode: 500, message: error.message });
-    });
-
-    req.end();
-  });
-}
