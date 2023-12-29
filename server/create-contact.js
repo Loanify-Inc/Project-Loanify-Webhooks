2f6dfde668a797d2439b8ec7250ec133c0c9fec8
William Boomer Baker <boomer@boomerbaker.com>
2023-12-29 03:43:10 -0800

    Fixing bate of birth format and state format

--- server/create-contact.js
+++ server/create-contact.js
@@ -1,5 +1,86 @@
 const https = require('https');
 
+// Function to convert state to capitalized abbreviation
+function getStateAbbreviation(state) {
+  const stateMap = {
+    alabama: 'AL',
+    alaska: 'AK',
+    arizona: 'AZ',
+    arkansas: 'AR',
+    california: 'CA',
+    colorado: 'CO',
+    connecticut: 'CT',
+    delaware: 'DE',
+    florida: 'FL',
+    georgia: 'GA',
+    hawaii: 'HI',
+    idaho: 'ID',
+    illinois: 'IL',
+    indiana: 'IN',
+    iowa: 'IA',
+    kansas: 'KS',
+    kentucky: 'KY',
+    louisiana: 'LA',
+    maine: 'ME',
+    maryland: 'MD',
+    massachusetts: 'MA',
+    michigan: 'MI',
+    minnesota: 'MN',
+    mississippi: 'MS',
+    missouri: 'MO',
+    montana: 'MT',
+    nebraska: 'NE',
+    nevada: 'NV',
+    newhampshire: 'NH',
+    newjersey: 'NJ',
+    newmexico: 'NM',
+    newyork: 'NY',
+    northcarolina: 'NC',
+    northdakota: 'ND',
+    ohio: 'OH',
+    oklahoma: 'OK',
+    oregon: 'OR',
+    pennsylvania: 'PA',
+    rhodeisland: 'RI',
+    southcarolina: 'SC',
+    southdakota: 'SD',
+    tennessee: 'TN',
+    texas: 'TX',
+    utah: 'UT',
+    vermont: 'VT',
+    virginia: 'VA',
+    washington: 'WA',
+    westvirginia: 'WV',
+    wisconsin: 'WI',
+    wyoming: 'WY',
+  };
+
+  // Convert the state to lowercase and get the abbreviation
+  const stateAbbreviation = state.toLowerCase();
+  return stateMap[stateAbbreviation] || '';
+}
+
+// Function to format date of birth as "YYYY-MM-DD"
+function formatDateOfBirth(dateOfBirth) {
+  // Try parsing the date in different formats
+  let parsedDate = new Date(dateOfBirth);
+  if (isNaN(parsedDate.getTime())) {
+    // Parsing failed, try another format
+    parsedDate = new Date(dateOfBirth.replace(/(\d{4})-(\d{2})-(\d{2})/, '$2/$3/$1'));
+  }
+
+  // Check if parsing succeeded
+  if (!isNaN(parsedDate.getTime())) {
+    const year = parsedDate.getFullYear();
+    const month = (parsedDate.getMonth() + 1).toString().padStart(2, '0'); // Months are 0-indexed
+    const day = parsedDate.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
+
+  // If parsing still failed, return an empty string
+  return '';
+}
+
 exports.handler = async (event, context) => {
   const API_KEY = '0db948a6-50f1-d9f3-4579-4f8036dc3830';
   const BASE_URL = 'api.forthcrm.com';
@@ -7,6 +88,12 @@ exports.handler = async (event, context) => {
 
   // Parse the incoming request body to extract contact data
   const body = JSON.parse(event.body);
+
+  // Convert state name to capitalized abbreviation
+  const stateAbbreviation = getStateAbbreviation(body.address.state);
+
+  // Format date of birth as "year-month-day"
+  const formattedDateOfBirth = formatDateOfBirth(body.date_of_birth);
 
   // Construct the request body for the Forth API call using the extracted data
   const forthRequestBody = {
@@ -21,12 +108,12 @@ exports.handler = async (event, context) => {
       address2: body.address.address2 || '', // Make address2 optional
       address3: body.address.address3 || '', // Make address3 optional
       city: body.address.city,
-      state: body.address.state,
+      state: stateAbbreviation, // Use the capitalized state abbreviation
       zip: body.address.zip,
     },
     email: body.email,
     social_security_number: body.social_security_number,
-    date_of_birth: body.date_of_birth,
+    date_of_birth: formattedDateOfBirth, // Use the formatted date of birth
     statusID: body.statusID,
     stageID: body.stageID,
     stage_label: body.stage_label,
@@ -109,6 +196,7 @@ function performHttpRequest(options, requestBody) {
     req.end();
   });
 }
+
 
 
 
