0b3c0642d9d3b47a4c18a919bc38b003f3f064a1
williamboomer87 <126490002+williamboomer87@users.noreply.github.com>
2023-12-29 22:45:10 +0530

    Update create-financial-report.js

--- server/create-financial-report.js
+++ server/create-financial-report.js
@@ -1,3 +1,84 @@
-exports.handler = async (event, context) => {
+const https = require('https');
 
+exports.handler = async (event, context) => {
+  const API_KEY = process.env.API_KEY;
+  const BASE_URL = 'api.forthcrm.com';
+
+  // Extract data from the request object
+  const {
+    firstName,
+    lastName,
+    preparedBy,
+    creditScore,
+    redFlagCodes,
+    debts,
+    creditUtilization,
+    totalDebt,
+    currentSituation,
+    debtModificationProgram,
+  } = JSON.parse(event.body);
+
+  // Add your logic to process the extracted data and construct the response object
+  const processedData = {
+    firstName,
+    lastName,
+    preparedBy,
+    creditScore,
+    redFlagCodes,
+    debts,
+    creditUtilization,
+    totalDebt,
+    currentSituation,
+    debtModificationProgram,
+    // Add other processed data as needed
+  };
+
+  // Generate financial report (replace this with your actual report generation logic)
+  const financialReportUrl = await generateFinancialReport(processedData);
+
+  // Construct the response object
+  const responseObject = {
+    financialReport: financialReportUrl,
+  };
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(responseObject),
+    headers: { 'Access-Control-Allow-Origin': '*' },
+  };
 };
+
+// Function to generate the financial report (Need to replace this with the actual report generation logic)
+async function generateFinancialReport(data) {
+  // Replace this with the logic to generate the financial report and store it in object storage
+  // Return the URL of the generated report in object storage
+  const reportUrl = "(object storage URL with a PDF of the generated report)";
+  return reportUrl;
+}
+
+// Rest of the code remains unchanged
+function performHttpRequest(options) {
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        if (res.statusCode === 200) {
+          resolve(data);
+        } else {
+          reject({ statusCode: res.statusCode, message: data });
+        }
+      });
+    });
+
+    req.on('error', (error) => {
+      reject({ statusCode: 500, message: error.message });
+    });
+
+    req.end();
+  });
+}
