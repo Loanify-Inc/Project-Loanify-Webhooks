f9db200e9c4e8932a4a8f0a5c4594d36a0807235
William-Boomer-Baker <williamboomer87@gmail.com>
2024-01-02 11:03:28 +0530

    Add update fix uncaught exception error

--- server/create-financial-report.js
+++ server/create-financial-report.js
@@ -4,18 +4,44 @@ const path = require('path');
 const os = require('os');
 
 // Function to create financial report and trigger webhook
-const createFinancialReport = async () => {
+exports.handler = async (event, context) => {
   try {
-    // ... (Generate financial report data)
-
-    const data = {
-      // ... (Generated financial report data)
+    const {
+      firstName,
+      lastName,
+      preparedBy,
+      creditScore,
+      redFlagCodes,
+      debts,
+      creditUtilization,
+      totalDebt,
+      currentSituation,
+      debtModificationProgram,
+    } = JSON.parse(event.body);
+  
+    // Add your logic to process the extracted data and construct the response object
+    const processedData = {
+      firstName,
+      lastName,
+      preparedBy,
+      creditScore,
+      redFlagCodes,
+      debts,
+      creditUtilization,
+      totalDebt,
+      currentSituation,
+      debtModificationProgram,
+      // Add other processed data as needed
     };
+
+    // const data = {
+    //   // ... (Generated financial report data)
+    // };
 
     // Save the data to a file in the /tmp directory
     const tempDir = os.tmpdir();
     const filePath = path.join(tempDir, 'financial-report.json');
-    fs.writeFileSync(filePath, JSON.stringify(data));
+    fs.writeFileSync(filePath, JSON.stringify(processedData));
 
     // Trigger the /receive-financial-report function with a webhook
     const webhookUrl = 'https://harmonious-mike.netlify.app/.netlify/functions/receive-financial-report';
